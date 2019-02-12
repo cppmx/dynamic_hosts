f613c2d9b2aff6c9c0c1004f7fabd6cee7536081
Carlos Colon <carlos.colon2@dxc.com>
2019-02-12 12:42:59 -0600

    Added argument parser
    Signed-off-by: Carlos Colon <carlos.colon2@dxc.com>

--- hosts.py
+++ hosts.py
@@ -9,13 +9,22 @@ Changes:
     06/02/2019     CECR     Initial version
 """
 
-from dynamic_hosts.logger import logger
+import argparse
+
 
 if __name__ == "__main__":
-    log = logger.Logger()
+    parser = argparse.ArgumentParser(description='Dynamic host generating tool')
+    parser.add_argument('--config', action='store_true', help='Display current configuration')
+    parser.add_argument('--env', choices=['dev', 'test', 'prod'],
+                        help='Execution environment of this script. By default it is executed in production.')
+    parser.add_argument('--list', action='store_true',
+                        help='Returns all hosts that meet the criteria.')
+    parser.add_argument('--new-server', action='store_true', help='Add new server record.')
+    parser.add_argument('--test', action='store_true', help='Run tests')
+    parser.add_argument('--update-server', action='store_true', help='Update information of a server.')
+    parser.add_argument('--verbose', '-v', action='count',
+                        help='Displays extra data in the console output. It should not be used in production.')
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
-    log.log_info("Test")
-    log.log_error("Test")
-    log.log_warning("Test")
-    log.log_verbose("Test")
+    args = parser.parse_args()
 
